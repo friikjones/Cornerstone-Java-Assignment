898ef91882a7d81446c73ff69408ff3230e2262a
friikjones <d.mullerjones@gmail.com>
2019-11-12 10:04:57 -0800

    Added argument printout

--- src/cli/CLIapplication.java
+++ src/cli/CLIapplication.java
@@ -4,6 +4,9 @@ public class CLIapplication {
 	public static void main(String[] arg0) {
 		if(arg0.length > 0) {
 			System.out.println(arg0.length);
+			for (int i = 0; i < arg0.length; i++) {
+				System.out.println("Argument "+i+": "+arg0[i]);
+			}
 		}else {
 			System.out.println("No arguments");
 		}
